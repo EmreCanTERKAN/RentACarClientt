d962577493fe7117efe310d8cec33626532dc88c
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-07-23 13:48:48 +0300

    content kısmı generic oldu artık

---
+++ apps/admin/src/components/blank/blank.ts
@@ -0,0 +1,31 @@
+import { DatePipe, Location, NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, input, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { EntityModel } from '../../models/entity.model';
+import Dashboard from "../../pages/dashboard/dashboard";
+
+@Component({
+  selector: 'app-blank',
+  imports: [NgClass, RouterLink, DatePipe],
+  templateUrl: './blank.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class Blank {
+  readonly pageIcon = input.required<string>();
+  readonly pageTitle = input.required<string>();
+  readonly pageDescription = input<string>('');
+  readonly status = input<boolean>(false);
+  readonly showStatus = input<boolean>(false);
+  readonly showBackBtn = input<boolean>(true);
+  readonly showEditBtn = input<boolean>(false);
+  readonly editBtnUrl = input<string>('');
+  readonly audit = input<EntityModel>();
+  readonly showAudit = input<boolean>(false);
+
+  readonly #location = inject(Location);
+
+  back(){
+    this.#location.back();
+  }
+}
