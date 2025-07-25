1e5c6a5690540c78ffb1d65a5a0713bf31f9ae03
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-07-25 12:29:57 +0300

    error ve lolading yönetimi

--- apps/admin/src/pages/login/login.ts
+++ apps/admin/src/pages/login/login.ts
@@ -1,26 +1,35 @@
-import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, signal, ViewEncapsulation } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Result } from '../../models/result.model';
 import { Router } from '@angular/router';
 import { FormValidateDirective } from 'form-validate-angular';
 
 @Component({
-  imports: [FormsModule,FormValidateDirective],
+  imports: [FormsModule, FormValidateDirective],
   templateUrl: './login.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Login {
+  readonly loading = signal<boolean>(false);
 
   readonly #http = inject(HttpClient);
   readonly #router = inject(Router);
 
   login(form: NgForm) {
     if (!form.valid) return;
-    this.#http.post<Result<string>>("/rent/auth/login", form.value).subscribe(res => {
-      localStorage.setItem("response",res.data!);
-      this.#router.navigateByUrl("/");
+
+    this.loading.set(true);
+    this.#http.post<Result<string>>("/rent/auth/login", form.value).subscribe({
+      next: res => {
+        localStorage.setItem("response", res.data!);
+        this.#router.navigateByUrl("/");
+        this.loading.set(false);
+      },
+      error: (err: HttpErrorResponse) => {
+        this.loading.set(false);
+      }
     })
   }
 }
