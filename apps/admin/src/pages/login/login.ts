42244c43b9803b49a8a3b6a86490bad6957d9c7f
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-07-26 15:00:13 +0300

    mail gönderme başarıyla tamamlandı

--- apps/admin/src/pages/login/login.ts
+++ apps/admin/src/pages/login/login.ts
@@ -1,9 +1,9 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, inject, signal, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild, ViewEncapsulation } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Result } from '../../models/result.model';
 import { Router } from '@angular/router';
 import { FormValidateDirective } from 'form-validate-angular';
+import { FlexiToastService } from 'flexi-toast';
+import { HttpService } from '../../services/http';
 
 @Component({
   imports: [FormsModule, FormValidateDirective],
@@ -13,23 +13,29 @@ import { FormValidateDirective } from 'form-validate-angular';
 })
 export default class Login {
   readonly loading = signal<boolean>(false);
+  readonly email = signal<string>("");
 
-  readonly #http = inject(HttpClient);
+  readonly closeBtn = viewChild<ElementRef<HTMLButtonElement>>("modalCloseBtn");
+
+  readonly #http = inject(HttpService);
   readonly #router = inject(Router);
+  readonly #toast = inject(FlexiToastService);
 
   login(form: NgForm) {
     if (!form.valid) return;
 
     this.loading.set(true);
-    this.#http.post<Result<string>>("/rent/auth/login", form.value).subscribe({
-      next: res => {
-        localStorage.setItem("response", res.data!);
-        this.#router.navigateByUrl("/");
-        this.loading.set(false);
-      },
-      error: (err: HttpErrorResponse) => {
-        this.loading.set(false);
-      }
-    })
+    this.#http.post<string>(`/rent/auth/login`, form.value, (res) => {
+      localStorage.setItem("token",res);
+      this.#router.navigateByUrl("/");
+      this.loading.set(false);
+    }, () => this.loading.set(false));
+  }
+
+  forgotPassword(){
+    this.#http.post<string>(`/rent/auth/forgot-password/${this.email()}`,{}, (res) => {
+      this.#toast.showToast("Başarılı",res,"info");
+      this.closeBtn()!.nativeElement.click();
+    });
   }
 }
